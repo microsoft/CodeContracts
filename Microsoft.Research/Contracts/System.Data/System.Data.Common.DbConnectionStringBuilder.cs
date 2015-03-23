c027b1a4d01d50109c0046f18789b9a9f83708e1
tom-englert <mail@tom-englert.de>
2015-03-23 16:47:14 +0100

    Fix contracts in DbConnectionStringBuilder and DataTable

--- Microsoft.Research/Contracts/System.Data/System.Data.Common.DbConnectionStringBuilder.cs
+++ Microsoft.Research/Contracts/System.Data/System.Data.Common.DbConnectionStringBuilder.cs
@@ -1,15 +1,15 @@
 // CodeContracts
-// 
+//
 // Copyright (c) Microsoft Corporation
-// 
-// All rights reserved. 
-// 
+//
+// All rights reserved.
+//
 // MIT License
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
@@ -28,7 +28,7 @@ namespace System.Data.Common
     public DbConnectionStringBuilder(bool useOdbcRules) { }
 
     extern public bool BrowsableConnectionString { get; set; }
-    public string ConnectionString { 
+    public string ConnectionString {
       get
       {
         Contract.Ensures(Contract.Result<string>() != null);
@@ -45,7 +45,14 @@ namespace System.Data.Common
     extern public virtual bool IsFixedSize { get; }
     extern public virtual bool IsReadOnly { get; }
 
-     // Exceptions:
+
+    // Property Value
+    //  The value associated with the specified key. If the specified key is not found,
+    //  trying to get it returns a null reference (Nothing in Visual Basic), and trying to set it creates a new element using the specified key.
+    //  Passing a null (Nothing in Visual Basic) key throws an ArgumentNullException.
+    //  Assigning a null value removes the key/value pair.
+    //
+    // Exceptions:
     //   System.ArgumentNullException:
     //     keyword is a null reference (Nothing in Visual Basic).
     //
@@ -57,13 +64,11 @@ namespace System.Data.Common
     {
       get
       {
-        Contract.Ensures(Contract.Result<object>() != null);
         return null;
       }
-
       set
       {
-        Contract.Requires(value != null);
+        Contract.Requires(keyword != null);
       }
     }
 
@@ -86,22 +91,22 @@ namespace System.Data.Common
     //     has a fixed size.
     public void Add(string keyword, object value)
     {
-      Contract.Requires(keyword!= null);
+      Contract.Requires(keyword != null);
       Contract.Requires(!this.IsReadOnly);
       Contract.Requires(!this.IsFixedSize);
     }
-    
+
     public static void AppendKeyValuePair(StringBuilder builder, string keyword, string value) { }
-    
+
     public static void AppendKeyValuePair(StringBuilder builder, string keyword, string value, bool useOdbcRules) { }
- 
+
     public virtual void Clear() { }
     //
     // Summary:
     //     Clears the collection of System.ComponentModel.PropertyDescriptor objects
     //     on the associated System.Data.Common.DbConnectionStringBuilder.
     protected internal void ClearPropertyDescriptors() { }
-    
+
     // public virtual bool ContainsKey(string keyword);
 
     [Pure]
@@ -110,11 +115,18 @@ namespace System.Data.Common
     [Pure]
     protected virtual void GetProperties(Hashtable propertyDescriptors) { }
 
-    [Pure]
-    public virtual bool Remove(string keyword) { return false; }
+    public virtual bool Remove(string keyword)
+    {
+      Contract.Requires(keyword != null);
+      return false;
+    }
 
     [Pure]
-    public virtual bool ShouldSerialize(string keyword) { return false; }
+    public virtual bool ShouldSerialize(string keyword)
+    {
+      Contract.Requires(keyword != null);
+      return false;
+    }
 
     [Pure]
     public virtual bool TryGetValue(string keyword, out object value)
