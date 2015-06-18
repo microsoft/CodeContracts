e2f2bdc31ffd93364c8364e926063fd584fbb804
Sam Harwell <sam@tunnelvisionlabs.com>
2015-06-18 11:45:01 -0500

    Add missing copyright headers

--- Microsoft.Research/ContractAdornments/CSharp/VersionedServicesFactory.cs
+++ Microsoft.Research/ContractAdornments/CSharp/VersionedServicesFactory.cs
@@ -1,4 +1,7 @@
-﻿namespace ContractAdornments.CSharp
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace ContractAdornments.CSharp
 {
     using System;
     using System.Collections.Generic;
