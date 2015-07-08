485be2b972640a0401ae8cef6d6f891f138f753b
Sam Harwell <sam@tunnelvisionlabs.com>
2015-07-08 17:28:29 -0500

    Fix comment explaining test

--- Foxtrot/Tests/Sources/ComplexGeneric.cs
+++ Foxtrot/Tests/Sources/ComplexGeneric.cs
@@ -13,8 +13,11 @@
 // THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 //
-// We test here that ?? operators which are compiled to Dup if then pop else .. by C# do not cause
-// unwarranted warnings in the extractor.
+// This is a regression test for Microsoft/CodeContracts#20:
+//
+//   Unable to cast object of type 'System.Compiler.TypeParameter' to type 'System.Compiler.Class'.
+//
+// https://github.com/Microsoft/CodeContracts/issues/20
 //
 
 using System.Diagnostics.Contracts;
