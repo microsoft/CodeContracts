1883927822d0df07b4aa287825cd615bda491237
Francesco Logozzo <logozzo@microsoft.com>
2015-07-11 12:30:27 -0500

    Reformat code in Graphs
    This reformat operation used dotnet/codeformatter@1d719e4e with the default
    settings.

--- Microsoft.Research/Graphs/SingleEdgeGraph.cs
+++ Microsoft.Research/Graphs/SingleEdgeGraph.cs
@@ -1,16 +1,5 @@
-// CodeContracts
-// 
-// Copyright (c) Microsoft Corporation
-// 
-// All rights reserved. 
-// 
-// MIT License
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
 using System.Collections.Generic;
@@ -20,139 +9,139 @@ using System.Diagnostics.Contracts;
 
 namespace Microsoft.Research.Graphs
 {
-  /// <summary>
-  /// A specialized graph based on a successor adjacency list.
-  ///
-  /// This graph maintains only a single distinct edges between a pair of nodes.
-  /// It uses a composer provided on construction to compose edge information if 
-  /// multiple edges are added between the same node.
-  /// </summary>
-  public class SingleEdgeGraph<Node, Info> : IGraphBuilder<Node, Info>
-  {
-    // invariant: if there exists an edge n1 -> n2, then n2 is in the domain of the edgemap
-    // this is important as we use that domain to characterize the set of nodes in our graph.
-    // and some nodes may not have outgoing edges.
-
-    private struct Edge
+    /// <summary>
+    /// A specialized graph based on a successor adjacency list.
+    ///
+    /// This graph maintains only a single distinct edges between a pair of nodes.
+    /// It uses a composer provided on construction to compose edge information if 
+    /// multiple edges are added between the same node.
+    /// </summary>
+    public class SingleEdgeGraph<Node, Info> : IGraphBuilder<Node, Info>
     {
-      private readonly Node source;
-      private readonly Node target;
-      private readonly Info info;
+        // invariant: if there exists an edge n1 -> n2, then n2 is in the domain of the edgemap
+        // this is important as we use that domain to characterize the set of nodes in our graph.
+        // and some nodes may not have outgoing edges.
 
-      public Edge(Node source, Info info, Node target)
-      {
-        this.source = source;
-        this.target = target;
-        this.info = info;
-      }
-
-      public Node Source
-      {
-        get { return this.source; }
-      }
-      public Node Target
-      {
-        get { return this.target; }
-      }
-      public Info Info { get { return this.info; } }
-    }
-
-    [ContractInvariantMethod]
-    private void ObjectInvariant()
-    {
-      Contract.Invariant(this.edgemap != null);
-    }
-
-
-    private readonly Dictionary<Node, Dictionary<Node, Edge>/*!*/>/*!*/ edgemap;
-    private readonly DComposer<Info, Info>/*!*/ composer;
-    private readonly IEqualityComparer<Node> nodeComparer;
-
-    private void Ignore(object ignore) { }
-
-    public SingleEdgeGraph(DComposer<Info, Info> edgeComposer, IEqualityComparer<Node> nodeComparer = null)
-    {
-      this.edgemap = new Dictionary<Node, Dictionary<Node, Edge>/*!*/>(nodeComparer);
-      this.composer = edgeComposer;
-      this.nodeComparer = nodeComparer;
-    }
-
-    public void AddNode(Node node)
-    {
-      // just materialize the node and edge list as a side
-      // effect of calling Edges
-      Ignore(Edges(node));
-    }
-
-    public void AddEdge(Node source, Info info, Node target)
-    {
-      Dictionary<Node, Edge> edges = Edges(source);
-      Edge existing;
-      if (edges.TryGetValue(target, out existing))
-      {
-        // compose info or ignore new edge
-        if (this.composer != null)
+        private struct Edge
         {
-          Info composed = this.composer(info, existing.Info);
-          edges[target] = new Edge(source, composed, target);
+            private readonly Node _source;
+            private readonly Node _target;
+            private readonly Info _info;
+
+            public Edge(Node source, Info info, Node target)
+            {
+                _source = source;
+                _target = target;
+                _info = info;
+            }
+
+            public Node Source
+            {
+                get { return _source; }
+            }
+            public Node Target
+            {
+                get { return _target; }
+            }
+            public Info Info { get { return _info; } }
         }
-      }
-      else
-      {
-        Edge edge = new Edge(source, info, target);
-        edges[target] = edge;
-        // Make sure target node is in graph
-        this.AddNode(target);
-      }
-    }
 
-    public SingleEdgeGraph<Node, Info> Reverse()
-    {
-      Contract.Ensures(Contract.Result<SingleEdgeGraph<Node, Info>>() != null);
-
-      var result = new SingleEdgeGraph<Node, Info>(this.composer, this.nodeComparer);
-      foreach (var pair in this.edgemap)
-      {
-        Contract.Assume(pair.Value != null);
-        foreach (var pair2 in pair.Value)
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
         {
-          result.AddEdge(pair2.Key, pair2.Value.Info, pair.Key);
+            Contract.Invariant(_edgemap != null);
         }
-      }
 
-      return result;
-    }
 
-    private Dictionary<Node, Edge>/*!*/ Edges(Node node)
-    {
-      Contract.Ensures(Contract.Result<Dictionary<Node, Edge>>() != null);
+        private readonly Dictionary<Node, Dictionary<Node, Edge>/*!*/>/*!*/ _edgemap;
+        private readonly DComposer<Info, Info>/*!*/ _composer;
+        private readonly IEqualityComparer<Node> _nodeComparer;
 
-      Dictionary<Node, Edge> result;
-      if (!this.edgemap.TryGetValue(node, out result))
-      {
-        result = new Dictionary<Node, Edge>(nodeComparer);
-        this.edgemap[node] = result;
-      }
-      else
-      {
-        Contract.Assume(result != null);
-      }
+        private void Ignore(object ignore) { }
 
-      //^ assert result != null;
-      return result;
-    }
+        public SingleEdgeGraph(DComposer<Info, Info> edgeComposer, IEqualityComparer<Node> nodeComparer = null)
+        {
+            _edgemap = new Dictionary<Node, Dictionary<Node, Edge>/*!*/>(nodeComparer);
+            _composer = edgeComposer;
+            _nodeComparer = nodeComparer;
+        }
 
-    #region IGraph<Node,Edge> Members
+        public void AddNode(Node node)
+        {
+            // just materialize the node and edge list as a side
+            // effect of calling Edges
+            Ignore(Edges(node));
+        }
 
-    public IEnumerable<Node>/*!*/ Nodes
-    {
-      get { return this.edgemap.Keys; }
-    }
+        public void AddEdge(Node source, Info info, Node target)
+        {
+            Dictionary<Node, Edge> edges = Edges(source);
+            Edge existing;
+            if (edges.TryGetValue(target, out existing))
+            {
+                // compose info or ignore new edge
+                if (_composer != null)
+                {
+                    Info composed = _composer(info, existing.Info);
+                    edges[target] = new Edge(source, composed, target);
+                }
+            }
+            else
+            {
+                Edge edge = new Edge(source, info, target);
+                edges[target] = edge;
+                // Make sure target node is in graph
+                this.AddNode(target);
+            }
+        }
 
-    public bool Contains(Node node)
-    {
-      return this.edgemap.ContainsKey(node);
-    }
+        public SingleEdgeGraph<Node, Info> Reverse()
+        {
+            Contract.Ensures(Contract.Result<SingleEdgeGraph<Node, Info>>() != null);
+
+            var result = new SingleEdgeGraph<Node, Info>(_composer, _nodeComparer);
+            foreach (var pair in _edgemap)
+            {
+                Contract.Assume(pair.Value != null);
+                foreach (var pair2 in pair.Value)
+                {
+                    result.AddEdge(pair2.Key, pair2.Value.Info, pair.Key);
+                }
+            }
+
+            return result;
+        }
+
+        private Dictionary<Node, Edge>/*!*/ Edges(Node node)
+        {
+            Contract.Ensures(Contract.Result<Dictionary<Node, Edge>>() != null);
+
+            Dictionary<Node, Edge> result;
+            if (!_edgemap.TryGetValue(node, out result))
+            {
+                result = new Dictionary<Node, Edge>(_nodeComparer);
+                _edgemap[node] = result;
+            }
+            else
+            {
+                Contract.Assume(result != null);
+            }
+
+            //^ assert result != null;
+            return result;
+        }
+
+        #region IGraph<Node,Edge> Members
+
+        public IEnumerable<Node>/*!*/ Nodes
+        {
+            get { return _edgemap.Keys; }
+        }
+
+        public bool Contains(Node node)
+        {
+            return _edgemap.ContainsKey(node);
+        }
 
 #if false
         public IEnumerable<IEdge<Node, EdgeInfo>/*!*/>/*!*/ Successors(Node node)
@@ -174,29 +163,28 @@ namespace Microsoft.Research.Graphs
             yield break;
         }
 #endif
-    #endregion
+        #endregion
 
 
-    #region IGraph<Node,Edge> Members
+        #region IGraph<Node,Edge> Members
 
 
-    public IEnumerable<Pair<Info, Node>> Successors(Node node)
-    {
-      Dictionary<Node, Edge> targets;
-      if (this.edgemap.TryGetValue(node, out targets))
-      {
-        foreach (Edge edge in targets.Values)
+        public IEnumerable<Pair<Info, Node>> Successors(Node node)
         {
-          yield return new Pair<Info, Node>(edge.Info, edge.Target);
+            Dictionary<Node, Edge> targets;
+            if (_edgemap.TryGetValue(node, out targets))
+            {
+                foreach (Edge edge in targets.Values)
+                {
+                    yield return new Pair<Info, Node>(edge.Info, edge.Target);
+                }
+            }
+            else
+            {
+                var n = node;
+            }
         }
-      }
-      else
-      {
-        var n = node;
-      }
+
+        #endregion
     }
-
-    #endregion
-  }
-
 }
