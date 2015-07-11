7d5348c4f21f7aa44080c5e5904389e181e468ab
Sergey Tepliakov <seteplia@microsoft.com>
2015-07-10 18:03:02 -0700

    Update version for v.1.9.10710.1 release.

--- Microsoft.Research/common/include/version.cs
+++ Microsoft.Research/common/include/version.cs
@@ -1,9 +1,9 @@
 // Warning: Automatically generated file. DO NOT EDIT
-// Generated at Thursday, July 9, 2015 3:41:56 PM
+// Generated at Friday, July 10, 2015 5:56:22 PM
 
 using System.Reflection;
-[assembly: AssemblyVersion("1.9.10709.0")]
-[assembly: AssemblyFileVersion("1.9.10709.0")]
+[assembly: AssemblyVersion("1.9.10710.1")]
+[assembly: AssemblyFileVersion("1.9.10710.1")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyCopyright("(c) Microsoft Corporation.  All rights reserved.")]
 
