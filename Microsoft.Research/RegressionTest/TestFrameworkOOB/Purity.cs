9d5d046e873ab68196063f83e1f49b6f9ecbf668
tom-englert <mail@tom-englert.de>
2015-07-18 15:31:31 +0200

    Added used test sources from TestFrameworkOOB to the project. Formatted code (no other changes).

--- Microsoft.Research/RegressionTest/TestFrameworkOOB/Purity.cs
+++ Microsoft.Research/RegressionTest/TestFrameworkOOB/Purity.cs
@@ -17,60 +17,61 @@ using System.Diagnostics.Contracts;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using Microsoft.Research.ClousotRegression;
 
 namespace TestFrameworkOOB.Purity
 {
-  class Tests
-  {
-    [ClousotRegressionTest]
-    public static void Test(object a, object b)
+    internal class Tests
     {
-      Contract.Requires(Object.ReferenceEquals(a, b));
+        [ClousotRegressionTest]
+        public static void Test(object a, object b)
+        {
+            Contract.Requires(ReferenceEquals(a, b));
+        }
 
+        [ClousotRegressionTest]
+        [RegressionOutcome(Outcome = ProofOutcome.Top, Message = "Possibly calling a method on a null reference \'dict\'", PrimaryILOffset = 3, MethodILOffset = 0), RegressionOutcome(Outcome = ProofOutcome.True, Message = "valid non-null reference (as receiver)", PrimaryILOffset = 16, MethodILOffset = 0)]
+        [RegressionOutcome(Outcome = ProofOutcome.True, Message = "assert is valid", PrimaryILOffset = 21, MethodILOffset = 0)]
+        public static void Test(IDictionary<int, string> dict, int key)
+        {
+            Contract.Requires(dict.ContainsKey(key));
+
+            Contract.Assert(dict.ContainsKey(key));
+        }
     }
 
-    [ClousotRegressionTest]
-    [RegressionOutcome(Outcome = ProofOutcome.Top, Message = "Possibly calling a method on a null reference \'dict\'", PrimaryILOffset = 3, MethodILOffset = 0), RegressionOutcome(Outcome = ProofOutcome.True, Message = "valid non-null reference (as receiver)", PrimaryILOffset = 16, MethodILOffset = 0)]
-    [RegressionOutcome(Outcome = ProofOutcome.True, Message = "assert is valid", PrimaryILOffset = 21, MethodILOffset = 0)]
-    public static void Test(IDictionary<int, string> dict, int key)
+    internal interface J
     {
-      Contract.Requires(dict.ContainsKey(key));
-
-      Contract.Assert(dict.ContainsKey(key));
-    }
-  }
-
-  interface J { }
-
-  class TypeMethodPurity : J
-  {
-    void Get(Type messageType)
-    {
-      Contract.Requires(messageType != null && typeof(J).IsAssignableFrom(messageType));
     }
 
-    void Foo()
+    internal class TypeMethodPurity : J
     {
-      J message = new TypeMethodPurity();
-      Type t = message.GetType();
-      Contract.Assert(t != null);
-      Contract.Assume(t == typeof(TypeMethodPurity));
-      Contract.Assume(typeof(J).IsAssignableFrom(typeof(TypeMethodPurity)));
-      Contract.Assume(typeof(J).IsAssignableFrom(t));
+        private void Get(Type messageType)
+        {
+            Contract.Requires(messageType != null && typeof (J).IsAssignableFrom(messageType));
+        }
+
+        private void Foo()
+        {
+            J message = new TypeMethodPurity();
+            Type t = message.GetType();
+            Contract.Assert(t != null);
+            Contract.Assume(t == typeof (TypeMethodPurity));
+            Contract.Assume(typeof (J).IsAssignableFrom(typeof (TypeMethodPurity)));
+            Contract.Assume(typeof (J).IsAssignableFrom(t));
+        }
+
+        [ClousotRegressionTest]
+        [RegressionOutcome(Outcome = ProofOutcome.True, Message = "valid non-null reference (as receiver)", PrimaryILOffset = 25, MethodILOffset = 0)]
+        [RegressionOutcome(Outcome = ProofOutcome.True, Message = "valid non-null reference (as receiver)", PrimaryILOffset = 47, MethodILOffset = 0)]
+        [RegressionOutcome(Outcome = ProofOutcome.True, Message = "assert is valid", PrimaryILOffset = 52, MethodILOffset = 0)]
+        private void Bar(Type t)
+        {
+            Contract.Requires(t != null);
+            Contract.Requires(typeof (J).IsAssignableFrom(t));
+
+            Contract.Assert(typeof (J).IsAssignableFrom(t));
+        }
     }
-
-    [ClousotRegressionTest]
-    [RegressionOutcome(Outcome = ProofOutcome.True, Message = "valid non-null reference (as receiver)", PrimaryILOffset = 25, MethodILOffset = 0)]
-    [RegressionOutcome(Outcome = ProofOutcome.True, Message = "valid non-null reference (as receiver)", PrimaryILOffset = 47, MethodILOffset = 0)]
-    [RegressionOutcome(Outcome = ProofOutcome.True, Message = "assert is valid", PrimaryILOffset = 52, MethodILOffset = 0)]
-    void Bar(Type t)
-    {
-      Contract.Requires(t != null);
-      Contract.Requires(typeof(J).IsAssignableFrom(t));
-
-      Contract.Assert(typeof(J).IsAssignableFrom(t));
-
-    }
-  }
 }
