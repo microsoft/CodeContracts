1e4419403254c2e8d5b1c7b4da7b501a6f0df9bd
Nathan Dykman <nathan.dykman@act-on.net>
2015-09-01 14:55:41 -0700

    Restoring original System.Windows.Forms.TabControl.cs source from master.

--- Microsoft.Research/Contracts/System.Windows.Forms/System.Windows.Forms.TabControl.cs
+++ Microsoft.Research/Contracts/System.Windows.Forms/System.Windows.Forms.TabControl.cs
@@ -92,6 +92,7 @@ namespace System.Windows.Forms
 
   public class TabControl
   {
+
     // Summary:
     //     Initializes a new instance of the System.Windows.Forms.TabControl class.
     public TabControl()
